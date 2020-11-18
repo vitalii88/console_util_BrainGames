24d72b66f532519c640cf4e64b60fa78563111ef
Vitalii Bairak <vitalii_b88@yahoo.com>
2020-11-18 21:27:01 +0200

    bug-fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import startGame from '../src/cli.js';
+import startCli from '../src/cli.js';
 
-startGame();
+startCli();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import promptly from 'promptly';
 
-const startGame = async () => {
-  const name = await promptly.prompt('Welcome to the Brain Games! \nMay I have your name? ');
-  return console.log('Hello, ', name, '!');
-};
-export default startGame;
+export default async function startCli() {
+  console.log('Welcome to the Brain Games!');
+  const name = await promptly.prompt('May I have your name?');
+  console.log(`Hello, ${name}!`);
+}
