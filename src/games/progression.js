a36f290e4489fd95f5aaebdf63288e62141d5b34
Vitalii Bairak <vitalii_b88@yahoo.com>
2020-12-03 18:02:53 +0200

    refactoring cod on progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,9 +4,8 @@ import startGame, { NUMBER_OF_ROUNDS } from '../index.js';
 const rule = 'What number is missing in the progression?';
 const progressionLength = 10;
 
-function getProgression() {
-  let iterationOfProgression = getRandomInt();
-  const progressionStep = getRandomInt(1, 30);
+function getProgression(beginningOfProgression, progressionStep) {
+  let iterationOfProgression = beginningOfProgression;
   const progression = [];
   for (let i = 0; i < progressionLength; i += 1) {
     progression.push(iterationOfProgression);
@@ -25,7 +24,9 @@ function hideIndexInProgress(arr, index) {
 export default function progressionGame() {
   const answerAndQuestion = [];
   for (let i = 0; i < NUMBER_OF_ROUNDS; i += 1) {
-    const progression = getProgression();
+    const iterationOfProgression = getRandomInt();
+    const progressionStep = getRandomInt(1, 30);
+    const progression = getProgression(iterationOfProgression, progressionStep);
     const replaceableIndex = getRandomInt(0, progressionLength - 1);
     const correctResult = progression[replaceableIndex].toString();
     const question = hideIndexInProgress(progression, replaceableIndex);
